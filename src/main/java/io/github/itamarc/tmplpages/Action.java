00ea0022fa74f676c9c692ad47212edb5b561b70
Itamar Carvalho <itamarc@gmail.com>
2021-06-25 15:15:41 -0300

    Showing the environment
    For tests purposes

--- src/main/java/io/github/itamarc/tmplpages/Action.java
+++ src/main/java/io/github/itamarc/tmplpages/Action.java
@@ -1,8 +1,20 @@
 package io.github.itamarc.tmplpages;
 
+import java.util.Map;
+
 public class Action {
     public static void main(String[] args) {
         TemplateProcessor proc = new TemplateProcessor(args[0], args[1]);
         System.out.println(proc.run());
+        printEnvironment();
+    }
+
+    private static void printEnvironment() {
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                              envName,
+                              env.get(envName));
+        }
     }
 }
