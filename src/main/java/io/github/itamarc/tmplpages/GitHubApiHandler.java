0410b32680ff258ca0c9d9baefecc897ae5596a6
Itamar <itamarc@gmail.com>
2021-06-25 01:08:14 -0300

    Preparing to get GH data

---
+++ src/main/java/io/github/itamarc/tmplpages/GitHubApiHandler.java
@@ -0,0 +1,11 @@
+package io.github.itamarc.tmplpages;
+
+import java.util.HashMap;
+
+public class GitHubApiHandler {
+    public HashMap<String, String> getRepositoryData(String repoName) {
+        HashMap<String, String> data = new HashMap<>();
+        // TODO: get the repository data and fill the map
+        return data;
+    }
+}
