7fb915e3cd91a507f3697003c93c1a83ff30979d
Itamar Carvalho <itamarc@gmail.com>
2021-06-25 15:31:30 -0300

    Improving output

--- src/main/java/io/github/itamarc/tmplpages/Action.java
+++ src/main/java/io/github/itamarc/tmplpages/Action.java
@@ -4,17 +4,21 @@ import java.util.Map;
 
 public class Action {
     public static void main(String[] args) {
+        System.out.println(">>> Received arguments:");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+        System.out.println(">>> Template processing:");
         TemplateProcessor proc = new TemplateProcessor(args[0], args[1]);
         System.out.println(proc.run());
         printEnvironment();
     }
 
     private static void printEnvironment() {
+        System.out.println(">>> Environment:");
         Map<String, String> env = System.getenv();
         for (String envName : env.keySet()) {
-            System.out.format("%s=%s%n",
-                              envName,
-                              env.get(envName));
+            System.out.format("%s=%s%n", envName, env.get(envName));
         }
     }
 }
