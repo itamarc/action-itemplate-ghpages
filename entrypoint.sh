79542d080f2e5b010d2a4c2d23ec16582b0c2cd8
Itamar Carvalho <itamarc@gmail.com>
2021-06-28 18:21:28 -0300

    Install git to publish

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Run the Action main code to generate the pages
 java -jar /usr/local/lib/action-itemplate-ghpages.jar
 
+# git is needed for the publish process
+apt-get -y install git
+
 # Publish the pages int the destination branch and folder
 chmod +x /usr/local/bin/publish.sh
 /usr/local/bin/publish.sh
