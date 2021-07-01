7f07f003bf6f0fc254d29d86026b409e7394f80b
Itamar Carvalho <itamarc@gmail.com>
2021-07-01 00:32:22 -0300

    Data from GH API properly formatted
    Fix #5

--- src/main/java/io/github/itamarc/tmplpages/GitHubApiHandler.java
+++ src/main/java/io/github/itamarc/tmplpages/GitHubApiHandler.java
@@ -15,66 +15,85 @@ import org.apache.http.impl.client.HttpClients;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * Class responsible to get data from GitHub API and put the values formatted
+ * in the data map that will be used to fill the templates.
+ */
 public class GitHubApiHandler {
+    /**
+     * Connect to the GitHub API, get the data and feed the map.
+     * @param valuesMap HashMap where the collected data will be inserted.
+     */
     public void getRepositoryData(HashMap<String, String> valuesMap) {
-        HashMap<String, String> dataMap = new HashMap<>();
+        // Get the data from the API
         JSONObject dataJson = callingGraph(valuesMap).getJSONObject("data").getJSONObject("repository");
 
-        processJsonObject(dataJson, dataMap, "repository_");
-
-        // Insert the data received into the existing valuesMap
-        for (String key : dataMap.keySet()) {
-            valuesMap.put(key, dataMap.get(key));
-        }
+        // Process the received data and insert into the map
+        processJsonObject(dataJson, valuesMap, "repository");
     }
 
     private void processJsonObject(JSONObject dataJson, HashMap<String, String> dataMap, String prefix) {
         for (String key : dataJson.keySet()) {
             Object obj = dataJson.get(key);
-            String currkey = prefix + key;
+            String currkey = prefix + "_" + key;
             if (isBaseType(obj.getClass())) {
                 dataMap.put(currkey, obj.toString());
                 System.out.println(currkey + " = " + obj.toString());
             } else if (obj instanceof JSONArray) {
-                if (key.equals("nodes") || key.equals("edges")) {
-                    processJsonArray((JSONArray) obj, dataMap, prefix);
-                } else {
-                    processJsonArray((JSONArray) obj, dataMap, currkey + "_");
-                }
                 System.out.println(currkey + ": JSONArray");
+                if (key.equals("nodes") || key.equals("edges")) {
+                    dataMap.put(prefix, processJsonArray((JSONArray) obj, dataMap, prefix));
+                } else {
+                    dataMap.put(currkey, processJsonArray((JSONArray) obj, dataMap, currkey));
+                }
             } else if (obj instanceof JSONObject) {
-                if (key.equals("node")) {
+                System.out.println(currkey + ": JSONObject");
+                if (key.equals("node") || key.equals("nodes")) {
                     processJsonObject((JSONObject) obj, dataMap, prefix);
                 } else {
-                    processJsonObject((JSONObject) obj, dataMap, currkey + "_");
+                    processJsonObject((JSONObject) obj, dataMap, currkey);
                 }
-                System.out.println(currkey + ": JSONObject");
             } else {
                 System.out.println("!!!>" + currkey + ": " + obj.getClass().getName());
             }
         }
     }
 
-    private void processJsonArray(JSONArray array, HashMap<String, String> dataMap, String prefix) {
-        for (int i = 0; i < array.length(); i++) {
-            Object obj = array.get(i);
-            String currkey = prefix + i;
-            if (isBaseType(obj.getClass())) {
-                dataMap.put(prefix + i, obj.toString());
-                System.out.println(currkey + " = " + obj.toString());
-            } else if (obj instanceof JSONArray) {
-                processJsonArray((JSONArray) obj, dataMap, currkey + "_");
-                System.out.println(currkey + ": JSONArray");
-            } else if (obj instanceof JSONObject) {
-                processJsonObject((JSONObject) obj, dataMap, currkey + "_");
-                System.out.println(currkey + ": JSONObject");
-            } else {
-                System.out.println("!!!>" + currkey + ": " + obj.getClass().getName());
+    private String processJsonArray(JSONArray array, HashMap<String, String> dataMap, String prefix) {
+        StringBuffer buffer = new StringBuffer();
+        if (prefix.equals("repository_licenseInfo_conditions")) {
+            buffer.append("[\"");
+            for (int i = 0; i < array.length(); i++) {
+                if (buffer.length() > 2) {
+                    buffer.append("\",\"");
+                }
+                JSONObject jso = array.getJSONObject(i);
+                String cond = jso.getString("label");
+                buffer.append(cond);
             }
+            buffer.append("\"]");
+        } else if (prefix.equals("repository_languages")) {
+            buffer.append("[");
+            for (int i = 0; i < array.length(); i++) {
+                if (buffer.length() > 1) {
+                    buffer.append(",");
+                }
+                JSONObject jso = array.getJSONObject(i);
+                JSONObject node = jso.getJSONObject("node");
+                node.put("size", jso.getInt("size"));
+                buffer.append(node.toString());
+            }
+            buffer.append("]");
+        } else { // repository_issues, repository_collaborators
+            buffer.append(array.toString());
         }
+        // TODO: remove prints only for testing
+        System.out.println("JSONArray (prefix '"+prefix+"'):");
+        System.out.println(buffer.toString());
+        return buffer.toString();
     }
 
-    public JSONObject callingGraph(HashMap<String, String> valuesMap) {
+    private JSONObject callingGraph(HashMap<String, String> valuesMap) {
         CloseableHttpClient client = null;
         CloseableHttpResponse response = null;
 
