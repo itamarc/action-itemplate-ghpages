4475df6408bed40698bca7239c47a13608a7f3b9
Itamar Carvalho <itamarc@gmail.com>
2021-09-20 16:16:43 -0300

    MarkdownProcessor now creates anchorlinks

--- src/main/java/io/github/itamarc/tmplpages/MarkdownProcessor.java
+++ src/main/java/io/github/itamarc/tmplpages/MarkdownProcessor.java
@@ -5,6 +5,7 @@ import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.anchorlink.AnchorLinkExtension;
 import com.vladsch.flexmark.ext.emoji.EmojiExtension;
 import com.vladsch.flexmark.ext.emoji.EmojiShortcutType;
 import com.vladsch.flexmark.ext.emoji.internal.EmojiReference;
@@ -24,7 +25,10 @@ public class MarkdownProcessor {
         options.set(EmojiExtension.USE_SHORTCUT_TYPE, EmojiShortcutType.GITHUB);
         options.set(EmojiExtension.ROOT_IMAGE_PATH, EmojiReference.githubUrl);
         emojiExt.parserOptions(options);
-        options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create(), emojiExt));
+        options.set(AnchorLinkExtension.ANCHORLINKS_WRAP_TEXT, false);
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+            TablesExtension.create(), StrikethroughExtension.create(), emojiExt, AnchorLinkExtension.create()));
+        options.set(HtmlRenderer.GENERATE_HEADER_ID, true);
 
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
